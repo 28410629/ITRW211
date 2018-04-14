84f51ea94e314eecb057353ef48466df66c860a2
Unknown <savannahtfritze@gmail.com>
2018-04-14 12:26:23 +0200

    First Commit

--- ITRW211_Project/ITRW211_Project/FormAppleInsider.Designer.cs
+++ ITRW211_Project/ITRW211_Project/FormAppleInsider.Designer.cs
@@ -28,20 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstHeadlines = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // FormBoredPanda
+            // lstHeadlines
+            // 
+            this.lstHeadlines.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lstHeadlines.FormattingEnabled = true;
+            this.lstHeadlines.Location = new System.Drawing.Point(15, 208);
+            this.lstHeadlines.Name = "lstHeadlines";
+            this.lstHeadlines.Size = new System.Drawing.Size(403, 277);
+            this.lstHeadlines.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(402, 183);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(15, 492);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(403, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get most recent article headings from AppleInsider";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormAppleInsider
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "FormBoredPanda";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(433, 527);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lstHeadlines);
+            this.Name = "FormAppleInsider";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FormBoredPanda";
+            this.Text = "FormAppleInsider";
+            this.Load += new System.EventHandler(this.FormAppleInsider_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstHeadlines;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ITRW211_Project/ITRW211_Project/FormAppleInsider.cs
+++ ITRW211_Project/ITRW211_Project/FormAppleInsider.cs
@@ -16,5 +16,64 @@ namespace ITRW211_Project
         {
             InitializeComponent();
         }
+
+        private string GetHTMLData(string url)
+        {
+            using (var client = new System.Net.WebClient())
+                try
+                {
+                    return client.DownloadString(url);
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message + "\n\n" + err.StackTrace);
+                }
+            return null;
+        }
+
+        private List<string> ExtractHeadlines()
+        {
+            List<string> rList = new List<string>();
+            string mainData;
+        
+            if ((mainData = GetHTMLData("https://appleinsider.com/")) != null)
+                try
+                {
+                    string trimmedData = mainData.Substring(mainData.IndexOf("< h1 >< a href = "));
+                    trimmedData = trimmedData.Remove(trimmedData.LastIndexOf("</a></h1>"));
+                   
+                    while (trimmedData.Contains("<h1"))
+                    {
+                        string line = trimmedData.Substring(trimmedData.IndexOf("<h1"));
+                        line = line.Remove(line.IndexOf("<h1"));
+                        trimmedData = trimmedData.Replace(line, "");
+                       
+                        if (line.Contains("</a>"))
+                        {
+                            line = line.Remove(line.LastIndexOf("</a>"));
+                            line = line.Substring(line.LastIndexOf(">") + 1);
+                            rList.Add(line);
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("AppleInsider has changed to a different page format; please update your software.");
+                }
+            else MessageBox.Show("No data was downloaded; please check your Internet connection.");
+            return rList;
+        }
+
+        private void FormAppleInsider_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                lstHeadlines.Items.Clear();
+                lstHeadlines.Items.AddRange(ExtractHeadlines().ToArray());
+            }
     }
 }
